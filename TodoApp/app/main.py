f50b321c270864a5fcd6c99a5e945166184ac329
Biniyam Getachew <binigetachew18@gmail.com>
2025-08-03 11:55:17 +0300

    Add admin router with endpoints for managing todos and role-based access control

--- TodoApp/app/main.py
+++ TodoApp/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.database import engine, Base, SessionLocal
-from routers import auth , todos
+from routers import auth , todos , admin
 
 app = FastAPI()
 
@@ -8,3 +8,5 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router) # Include the auth router with a prefix
 app.include_router(todos.router) # Include the todos router with a prefix
+app.include_router(admin.router) # Include the admin router with a prefix
+
